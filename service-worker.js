68ab23ff7dd0e698796cb5610bcc4b18a43b6f60
XavierCarty95 <XavierCarty.student@careerdevs.com>
2019-09-16 03:38:22 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a01d2239604d7301c8fad17b18bc8b9e.js"
+  "/portfolio/precache-manifest.8fc2c67b83f7775cd2e7204a3b7df0b7.js"
 );
 
 self.addEventListener('message', (event) => {
