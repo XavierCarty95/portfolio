e1054a9a5a2111f6d0418c24ed33d69838e6ac3a
XavierCarty95 <XavierCarty.student@careerdevs.com>
2019-09-16 02:18:51 -0400

    Updates

---
+++ precache-manifest.9dcee4a255d410d6c84f3426d9c59955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df5ccc872a069fcaf281f52bfa469ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "534d66512c125642e3b7",
+    "url": "/portfolio/static/css/main.fee0edd7.chunk.css"
+  },
+  {
+    "revision": "ffa78b159ea08d1b01ad",
+    "url": "/portfolio/static/js/2.d1b2e4e1.chunk.js"
+  },
+  {
+    "revision": "534d66512c125642e3b7",
+    "url": "/portfolio/static/js/main.3819d589.chunk.js"
+  },
+  {
+    "revision": "4bc79a2d54f74fff90c2",
+    "url": "/portfolio/static/js/runtime~main.f7d4dbe8.js"
+  },
+  {
+    "revision": "a708e253bd7e0f105cf92e01515dc34b",
+    "url": "/portfolio/static/media/cs50x.a708e253.jpg"
+  },
+  {
+    "revision": "3344c9b0b4d8cd12cee8490538c131c7",
+    "url": "/portfolio/static/media/pic.3344c9b0.jpg"
+  }
+]);
